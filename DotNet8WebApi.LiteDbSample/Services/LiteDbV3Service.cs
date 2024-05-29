62d9ec32e9ba894d6cf7e616ae271ca267a24d21
Linn Thit <lth1212001@gmail.com>
2024-05-29 13:30:03 +0630

    -

--- DotNet8WebApi.LiteDbSample/Services/LiteDbV3Service.cs
+++ DotNet8WebApi.LiteDbSample/Services/LiteDbV3Service.cs
@@ -1,55 +1,54 @@
 ﻿using LiteDB;
 using System.Linq.Expressions;
 
-namespace DotNet8WebApi.LiteDbSample.Services
+namespace DotNet8WebApi.LiteDbSample.Services;
+
+public class LiteDbV3Service
 {
-    public class LiteDbV3Service
+    private readonly LiteDatabase _liteDatabase;
+
+    public LiteDbV3Service(LiteDatabase liteDatabase)
     {
-        private readonly LiteDatabase _liteDatabase;
+        _liteDatabase = liteDatabase;
+    }
 
-        public LiteDbV3Service(LiteDatabase liteDatabase)
-        {
-            _liteDatabase = liteDatabase;
-        }
+    public List<T> List<T>(string tableOrClassName)
+    {
+        tableOrClassName ??= typeof(T).Name;
+        ILiteCollection<T> lst = tableOrClassName is not null
+            ? _liteDatabase.GetCollection<T>(tableOrClassName)
+            : _liteDatabase.GetCollection<T>();
 
-        public List<T> List<T>(string tableOrClassName)
-        {
-            tableOrClassName ??= typeof(T).Name;
-            ILiteCollection<T> lst = tableOrClassName is not null
-                ? _liteDatabase.GetCollection<T>(tableOrClassName)
-                : _liteDatabase.GetCollection<T>();
+        List<T> _list = lst.FindAll().ToList();
+        return _list;
+    }
 
-            List<T> _list = lst.FindAll().ToList();
-            return _list;
-        }
+    public T GetById<T>(Expression<Func<T, bool>> condition, string tableOrClassName)
+    {
+        tableOrClassName ??= typeof(T).Name;
+        ILiteCollection<T> lst = tableOrClassName is not null
+            ? _liteDatabase.GetCollection<T>(tableOrClassName)
+            : _liteDatabase.GetCollection<T>();
+        var item = lst.Find(condition).FirstOrDefault();
 
-        public T GetById<T>(Expression<Func<T, bool>> condition, string tableOrClassName)
-        {
-            tableOrClassName ??= typeof(T).Name;
-            ILiteCollection<T> lst = tableOrClassName is not null
-                ? _liteDatabase.GetCollection<T>(tableOrClassName)
-                : _liteDatabase.GetCollection<T>();
-            var item = lst.Find(condition).FirstOrDefault();
+        return item!;
+    }
 
-            return item!;
-        }
+    public BsonValue Add<T>(T requestModel, string tableOrClassName)
+    {
+        tableOrClassName ??= typeof(T).Name;
+        return _liteDatabase.GetCollection<T>(tableOrClassName).Insert(requestModel);
+    }
 
-        public BsonValue Add<T>(T requestModel, string tableOrClassName)
-        {
-            tableOrClassName ??= typeof(T).Name;
-            return _liteDatabase.GetCollection<T>(tableOrClassName).Insert(requestModel);
-        }
+    public bool Update<T>(T requestModel, string tableOrClassName)
+    {
+        tableOrClassName ??= typeof(T).Name;
+        return _liteDatabase.GetCollection<T>(tableOrClassName).Update(requestModel);
+    }
 
-        public bool Update<T>(T requestModel, string tableOrClassName)
-        {
-            tableOrClassName ??= typeof(T).Name;
-            return _liteDatabase.GetCollection<T>(tableOrClassName).Update(requestModel);
-        }
-
-        public bool Delete<T>(ObjectId Id, string tableOrClassName)
-        {
-            tableOrClassName ??= typeof(T).Name;
-            return _liteDatabase.GetCollection<T>(tableOrClassName).Delete(new BsonValue(Id));
-        }
+    public bool Delete<T>(ObjectId Id, string tableOrClassName)
+    {
+        tableOrClassName ??= typeof(T).Name;
+        return _liteDatabase.GetCollection<T>(tableOrClassName).Delete(new BsonValue(Id));
     }
 }
