6c5b3b87abfc656699e843da950c94cdd02363ea
Linn Thit <lth1212001@gmail.com>
2024-05-29 12:03:49 +0630

    -

--- DotNet8WebApi.LiteDbSample/Program.cs
+++ DotNet8WebApi.LiteDbSample/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddControllers().AddJsonOptions(opt =>
 {
     opt.JsonSerializerOptions.PropertyNamingPolicy = null;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
