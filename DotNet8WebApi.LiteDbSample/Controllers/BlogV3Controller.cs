bf6bd41f24ca6661239041c6683a2c7b9fa50284
Linn Thit <lth1212001@gmail.com>
2024-05-29 13:29:25 +0630

    -

--- DotNet8WebApi.LiteDbSample/Controllers/BlogV3Controller.cs
+++ DotNet8WebApi.LiteDbSample/Controllers/BlogV3Controller.cs
@@ -2,105 +2,104 @@
 using DotNet8WebApi.LiteDbSample.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8WebApi.LiteDbSample.Controllers
+namespace DotNet8WebApi.LiteDbSample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogV3Controller : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogV3Controller : ControllerBase
+    private readonly LiteDbV3Service _liteDbV3Service;
+    private readonly string _tableName;
+
+    public BlogV3Controller(LiteDbV3Service liteDbV3Service)
     {
-        private readonly LiteDbV3Service _liteDbV3Service;
-        private readonly string _tableName;
+        _liteDbV3Service = liteDbV3Service;
+        _tableName = "Blog";
+    }
 
-        public BlogV3Controller(LiteDbV3Service liteDbV3Service)
+    [HttpGet]
+    public IActionResult GetBlogs()
+    {
+        var lst = _liteDbV3Service.List<BlogModel>(_tableName);
+        return Ok(lst);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlog(string id)
+    {
+        var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public IActionResult CreateBlog([FromBody] BlogRequestModel requestModel)
+    {
+        var blog = new BlogModel
         {
-            _liteDbV3Service = liteDbV3Service;
-            _tableName = "Blog";
-        }
+            BlogId = Guid.NewGuid().ToString(),
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
+        _liteDbV3Service.Add<BlogModel>(blog, _tableName);
 
-        [HttpGet]
-        public IActionResult GetBlogs()
+        return Ok(blog);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(string id, [FromBody] BlogRequestModel requestModel)
+    {
+        var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
+
+        if (item is null)
+            return NotFound("No data found.");
+
+        item.BlogTitle = requestModel.BlogTitle;
+        item.BlogAuthor = requestModel.BlogAuthor;
+        item.BlogContent = requestModel.BlogContent;
+
+        var result = _liteDbV3Service.Update<BlogModel>(item, _tableName);
+
+        return result ? StatusCode(202, "Updating Successful.") : BadRequest();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult Patch(string id, [FromBody] BlogRequestModel requestModel)
+    {
+        var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
+
+        if (item is null)
+            return NotFound("No data found.");
+
+        if (!string.IsNullOrEmpty(requestModel.BlogTitle))
         {
-            var lst = _liteDbV3Service.List<BlogModel>(_tableName);
-            return Ok(lst);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetBlog(string id)
-        {
-            var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
-            return Ok(item);
-        }
-
-        [HttpPost]
-        public IActionResult CreateBlog([FromBody] BlogRequestModel requestModel)
-        {
-            var blog = new BlogModel
-            {
-                BlogId = Guid.NewGuid().ToString(),
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-            _liteDbV3Service.Add<BlogModel>(blog, _tableName);
-
-            return Ok(blog);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Put(string id, [FromBody] BlogRequestModel requestModel)
-        {
-            var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
-
-            if (item is null)
-                return NotFound("No data found.");
-
             item.BlogTitle = requestModel.BlogTitle;
+        }
+
+        if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
+        {
             item.BlogAuthor = requestModel.BlogAuthor;
+        }
+
+        if (!string.IsNullOrEmpty(requestModel.BlogContent))
+        {
             item.BlogContent = requestModel.BlogContent;
-
-            var result = _liteDbV3Service.Update<BlogModel>(item, _tableName);
-
-            return result ? StatusCode(202, "Updating Successful.") : BadRequest();
         }
 
-        [HttpPatch("{id}")]
-        public IActionResult Patch(string id, [FromBody] BlogRequestModel requestModel)
-        {
-            var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
+        var result = _liteDbV3Service.Update<BlogModel>(item, _tableName);
 
-            if (item is null)
-                return NotFound("No data found.");
+        return result ? StatusCode(202, "Updating Successful.") : BadRequest();
+    }
 
-            if (!string.IsNullOrEmpty(requestModel.BlogTitle))
-            {
-                item.BlogTitle = requestModel.BlogTitle;
-            }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBlog(string id)
+    {
+        var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
+        if (item is null)
+            return NotFound("No data found.");
 
-            if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
-            {
-                item.BlogAuthor = requestModel.BlogAuthor;
-            }
+        var result = _liteDbV3Service.Delete<BlogModel>(item.Id!, _tableName);
 
-            if (!string.IsNullOrEmpty(requestModel.BlogContent))
-            {
-                item.BlogContent = requestModel.BlogContent;
-            }
-
-            var result = _liteDbV3Service.Update<BlogModel>(item, _tableName);
-
-            return result ? StatusCode(202, "Updating Successful.") : BadRequest();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBlog(string id)
-        {
-            var item = _liteDbV3Service.GetById<BlogModel>(x => x.BlogId == id, _tableName);
-            if (item is null)
-                return NotFound("No data found.");
-
-            var result = _liteDbV3Service.Delete<BlogModel>(item.Id!, _tableName);
-
-            return result ? StatusCode(202, "Deleting Successful.") : BadRequest();
-        }
+        return result ? StatusCode(202, "Deleting Successful.") : BadRequest();
     }
 }
