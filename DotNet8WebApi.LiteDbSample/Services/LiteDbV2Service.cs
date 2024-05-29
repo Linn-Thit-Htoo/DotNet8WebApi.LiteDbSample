52e510fccdd74c6b8cf06abe0def016184d9c210
Linn Thit <lth1212001@gmail.com>
2024-05-29 12:03:25 +0630

    -

--- DotNet8WebApi.LiteDbSample/Services/LiteDbV2Service.cs
+++ DotNet8WebApi.LiteDbSample/Services/LiteDbV2Service.cs
@@ -1,24 +1,23 @@
 ﻿using DotNet8WebApi.LiteDbSample.Models;
 using LiteDB;
 
-namespace DotNet8WebApi.LiteDbSample.Services
+namespace DotNet8WebApi.LiteDbSample.Services;
+
+public class LiteDbV2Service
 {
-    public class LiteDbV2Service
+    private readonly LiteDatabase _liteDatabase;
+
+    public LiteDbV2Service(LiteDatabase liteDatabase)
     {
-        private readonly LiteDatabase _liteDatabase;
-
-        public LiteDbV2Service(LiteDatabase liteDatabase)
-        {
-            _liteDatabase = liteDatabase;
-        }
-
-        //public ILiteCollection<BlogModel> Blogs()
-        //{
-        //    return _liteDatabase.GetCollection<BlogModel>("Blog");
-        //}
-
-        public ILiteCollection<BlogModel> Blogs => _liteDatabase.GetCollection<BlogModel>("Blog");
-
-        public void Dispose() => _liteDatabase.Dispose();
+        _liteDatabase = liteDatabase;
     }
+
+    //public ILiteCollection<BlogModel> Blogs()
+    //{
+    //    return _liteDatabase.GetCollection<BlogModel>("Blog");
+    //}
+
+    public ILiteCollection<BlogModel> Blogs => _liteDatabase.GetCollection<BlogModel>("Blog");
+
+    public void Dispose() => _liteDatabase.Dispose();
 }
