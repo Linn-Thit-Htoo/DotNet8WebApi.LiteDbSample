72a7e55d0fb00d582e5f2a1b7708df1d28185382
Linn Thit <lth1212001@gmail.com>
2024-05-29 13:39:06 +0630

    -

--- DotNet8WebApi.LiteDbSample/Controllers/BlogV2Controller.cs
+++ DotNet8WebApi.LiteDbSample/Controllers/BlogV2Controller.cs
@@ -78,6 +78,9 @@ public class BlogV2Controller : ControllerBase
 
     #endregion
 
+    #region Patch
+
+    #endregion
     [HttpPatch("{id}")]
     public IActionResult Patch(string id, [FromBody] BlogRequestModel requestModel)
     {
