f485a9f52265af5313ce6f2097e017390ca2adb2
Linn Thit <lth1212001@gmail.com>
2024-05-29 12:01:55 +0630

    -

--- DotNet8WebApi.LiteDbSample/Controllers/BlogController.cs
+++ DotNet8WebApi.LiteDbSample/Controllers/BlogController.cs
@@ -2,126 +2,125 @@
 using LiteDB;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8WebApi.LiteDbSample.Controllers
+namespace DotNet8WebApi.LiteDbSample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly string _filePath;
+    private readonly string _folderPath;
+
+    public BlogController()
     {
-        private readonly string _filePath;
-        private readonly string _folderPath;
+        _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
+        Directory.CreateDirectory(_folderPath);
+        _filePath = Path.Combine(_folderPath, "Blog.db");
+    }
 
-        public BlogController()
+    [HttpGet]
+    public IActionResult GetBlogs()
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var lst = collection.FindAll().ToList();
+        db.Dispose();
+
+        return Ok(lst);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlog(string id)
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
+        db.Dispose();
+
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] BlogRequestModel requestModel)
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var blog = new BlogModel
         {
-            _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
-            Directory.CreateDirectory(_folderPath);
-            _filePath = Path.Combine(_folderPath, "Blog.db");
-        }
+            BlogId = Guid.NewGuid().ToString(),
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
+        collection.Insert(blog);
+        db.Dispose();
 
-        [HttpGet]
-        public IActionResult GetBlogs()
+        return Ok(blog);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(string id, [FromBody] BlogRequestModel requestModel)
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
+
+        if (item is null)
+            return NotFound("No data found.");
+
+        item.BlogTitle = requestModel.BlogTitle;
+        item.BlogAuthor = requestModel.BlogAuthor;
+        item.BlogContent = requestModel.BlogContent;
+
+        var result = collection.Update(item);
+        db.Dispose();
+
+        return result ? StatusCode(202, "Updating Successful.") : BadRequest();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult Patch(string id, [FromBody] BlogRequestModel requestModel)
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
+
+        if (item is null)
+            return NotFound("No data found.");
+
+        if (!string.IsNullOrEmpty(requestModel.BlogTitle))
         {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var lst = collection.FindAll().ToList();
-            db.Dispose();
-
-            return Ok(lst);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetBlog(string id)
-        {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
-            db.Dispose();
-
-            return Ok(item);
-        }
-
-        [HttpPost]
-        public IActionResult Create([FromBody] BlogRequestModel requestModel)
-        {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var blog = new BlogModel
-            {
-                BlogId = Guid.NewGuid().ToString(),
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-            collection.Insert(blog);
-            db.Dispose();
-
-            return Ok(blog);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Put(string id, [FromBody] BlogRequestModel requestModel)
-        {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
-
-            if (item is null)
-                return NotFound("No data found.");
-
             item.BlogTitle = requestModel.BlogTitle;
+        }
+
+        if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
+        {
             item.BlogAuthor = requestModel.BlogAuthor;
+        }
+
+        if (!string.IsNullOrEmpty(requestModel.BlogContent))
+        {
             item.BlogContent = requestModel.BlogContent;
-
-            var result = collection.Update(item);
-            db.Dispose();
-
-            return result ? StatusCode(202, "Updating Successful.") : BadRequest();
         }
 
-        [HttpPatch("{id}")]
-        public IActionResult Patch(string id, [FromBody] BlogRequestModel requestModel)
-        {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
+        var result = collection.Update(item);
+        db.Dispose();
 
-            if (item is null)
-                return NotFound("No data found.");
+        return result ? StatusCode(202, "Updating Successful.") : BadRequest();
+    }
 
-            if (!string.IsNullOrEmpty(requestModel.BlogTitle))
-            {
-                item.BlogTitle = requestModel.BlogTitle;
-            }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBlog(string id)
+    {
+        var db = new LiteDatabase(_filePath);
+        var collection = db.GetCollection<BlogModel>("Blog");
+        var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
+        if (item is null)
+            return NotFound("No data found.");
 
-            if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
-            {
-                item.BlogAuthor = requestModel.BlogAuthor;
-            }
+        var result = collection.Delete(item.Id);
+        db.Dispose();
 
-            if (!string.IsNullOrEmpty(requestModel.BlogContent))
-            {
-                item.BlogContent = requestModel.BlogContent;
-            }
-
-            var result = collection.Update(item);
-            db.Dispose();
-
-            return result ? StatusCode(202, "Updating Successful.") : BadRequest();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBlog(string id)
-        {
-            var db = new LiteDatabase(_filePath);
-            var collection = db.GetCollection<BlogModel>("Blog");
-            var item = collection.Find(x => x.BlogId == id).FirstOrDefault();
-            if (item is null)
-                return NotFound("No data found.");
-
-            var result = collection.Delete(item.Id);
-            db.Dispose();
-
-            return result ? StatusCode(202, "Deleting Successful.") : BadRequest();
-        }
+        return result ? StatusCode(202, "Deleting Successful.") : BadRequest();
     }
 }
